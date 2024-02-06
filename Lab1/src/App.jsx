0d7f4fbddf584792b1e1d19f365ced9b53ae3e9f
Santiago Perez <santipr2004@gmail.com>
2024-02-06 18:09:13 -0500

    Feat: Explore section finish no styles

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import ExploreSection from './components/ExploreSection/ExploreSection'
 
 export default function App(){
     return(
       <>
       <h1>Hola mundo</h1>
-      
+      <ExploreSection></ExploreSection>
       </>
     )
 }
