a25af38ee9e45522cfa4964b431b59a0d5406f33
Santiago Perez <santipr2004@gmail.com>
2024-02-06 17:23:17 -0500

    Feat: NavBar and Main Section finish no styles

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainSections from './components/MainSections/MainSections'
+import NavBar from './components/NavBar/NavBar'
 
 export default function App(){
     return(
       <>
       <h1>Hola mundo</h1>
       <MainSections></MainSections>
+      <NavBar></NavBar>
       </>
     )
 }
