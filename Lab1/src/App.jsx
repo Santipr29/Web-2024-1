0e0c3e966295d4d6b97b03aba2aafb25f6cc38fb
Santiago <santipr2004@gmail.com>
2024-02-06 10:31:44 -0500

    Feat: NavBar component finish no styles

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar/NavBar'
 
 export default function App(){
     return(
       <>
       <h1>Hola mundo</h1>
+      <NavBar></NavBar>
       </>
     )
 }
--- Lab1/src/components/NavBar/NavBar.jsx
+++ Lab1/src/components/NavBar/NavBar.jsx
@@ -4,20 +4,20 @@ import { IconBtn } from '../IconBtn/IconBtn'
 export default function NavBar(){
     return(
       <>
-      <section class="main">
-        <section class="left">
-            <IconBtn></IconBtn>
+      <section className="main">
+        <section className="left">
+            <IconBtn image="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-5.png"></IconBtn>
+            <IconBtn image="https://ongpng.com/wp-content/uploads/2023/04/3.youtube-logo-2416x776-1.png"></IconBtn>
         </section>
-        <section class="center">
-            <input>
-                
-            </input>
-            <IconBtn></IconBtn>
+        <section className="center">
+            <input placeholder="Search"></input>
+            <IconBtn image="https://cdn.iconscout.com/icon/free/png-256/free-search-1990-1174913.png"></IconBtn>
+            <IconBtn image="https://pixsector.com/cache/fb36ec30/av5e4cb285ffdbbf48898.png"></IconBtn>
         </section>
-        <section class="right">
-            <IconBtn></IconBtn>
-            <IconBtn></IconBtn>
-            <IconBtn></IconBtn>
+        <section className="right">
+            <IconBtn image="https://cdn.iconscout.com/icon/free/png-256/free-upload-1438217-1216178.png"></IconBtn>
+            <IconBtn image="https://www.shareicon.net/data/512x512/2017/02/15/878700_alert_512x512.png"></IconBtn>
+            <IconBtn image="https://yt3.ggpht.com/I0yZzIyRQZ9PHPBA5yGbUnOHu6wt0vr1ONYvcwYwW2PKiVdCqOsibQ1579J5CAoS7BY_Mf7FHqo=s68-c-k-c0x00ffffff-no-rj"></IconBtn>
         </section>
       </section>
       </>
