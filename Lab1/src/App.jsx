5192f711c184535c49007b24c914623c942d6aad
Santiago Perez <santipr2004@gmail.com>
2024-02-06 17:56:18 -0500

    Feat: subscriptions section finish no styles

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import YourSection from './components/YourSection/YourSection'
+
 
 export default function App(){
     return(
       <>
       <h1>Hola mundo</h1>
-      <YourSection></YourSection>
+      
       </>
     )
 }
---
+++ Lab1/src/components/SubsSection/SubsSection.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { SectionBtn } from "../SectionBtn/SectionBtn"
+
+export default function SubsSection(){
+    return(
+      <>
+      <section>
+      <h2>Subscriptions</h2>
+      <SectionBtn image="https://yt3.googleusercontent.com/ytc/AIf8zZSmNKhvFf2fL7E8IF5pX_E3a1tE4BoigztO1KZSUF0=s176-c-k-c0x00ffffff-no-rj-mo" title="Pablots"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/_cJUz0aSSV6TtTrw4_XaqsMVjbMkddQxxLxnN2G_PJ9NZv-9sjVXC88ILPd7kQRkO8OO7YG7=s176-c-k-c0x00ffffff-no-rj-mo" title="NBA"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/4XDYWC0nsWGrDn__8NnDypC-5eS2gfhfst273Y_Huf1gnmOJ05zKH8uJlYAp7ejBpjX08lFoHOI=s176-c-k-c0x00ffffff-no-rj-mo" title="FC Barcelona"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/3ondwJtqgg_1YlfjFd_KJMQ-Q4bz764TiYlsZc0kKwsU5FQghNUwalM0jnnPo7WysTCOWkQQIg=s176-c-k-c0x00ffffff-no-rj-mo" title="ESPN Fans"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/pk-jnYnbdNBUKEtBdc3buYbCM0Xu0_TDcVfYON1qAez3CVPbA-FWGyCbF4BaRF1T9MqxjG-mugk=s176-c-k-c0x00ffffff-no-rj-mo" title="Ibai"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/U9x-_BMoXJBPf9RWEab_TEu2TX1o1plDjeY52_BQVshkLYDtiQkCo3MVZsmq5X0gf0cTvRkFig=s176-c-k-c0x00ffffff-no-rj-mo" title="Man City"></SectionBtn>
+      <SectionBtn image="https://yt3.googleusercontent.com/F9tU0eTy96fqLC1c8SGgU8GNqyRiyEQttOuY2epuCDr2XhyFoaMecugxA6kcUrEMqZL4HjnPUg=s176-c-k-c0x00ffffff-no-rj-mo" title="Dei V"></SectionBtn>
+      </section>
+      </>
+    )
+}
