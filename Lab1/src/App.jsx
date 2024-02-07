344719be99add73c92a0d05f6da07375e2b2eac7
Santiago Perez <santipr2004@gmail.com>
2024-02-06 19:12:54 -0500

    feat: Landing finish no styles

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import NavBar from './components/NavBar/NavBar'
+import MainSections from './components/MainSections/MainSections'
+import VideoSection from './components/VideoSection/VideoSection'
 
 export default function App(){
     return(
       <>
       <h1>Hola mundo</h1>
-      
+      <NavBar></NavBar>
+      <section>
+        <MainSections></MainSections>
+        <VideoSection></VideoSection>
+      </section>
       </>
     )
 }
