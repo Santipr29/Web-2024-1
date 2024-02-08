b66b4d68b587a480ec89e949136c4da1b185ef72
Santiago <santipr2004@gmail.com>
2024-02-08 17:03:30 -0500

    Feat: NavBar phone finish no styles

---
+++ Lab1/src/components/NavBarCel/NavBarCel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IconBtn } from '../IconBtn/IconBtn'
+import "./NavBar.css"
+
+export default function NavBarCel(){
+    return(
+      <>
+      <section className="main">
+        <section className="left">
+            <IconBtn className="yt" image="https://ongpng.com/wp-content/uploads/2023/04/3.youtube-logo-2416x776-1.png"></IconBtn>
+        </section>
+        <section className="right">
+            <IconBtn className="share" image="https://static.thenounproject.com/png/3388431-200.png"></IconBtn>
+            <IconBtn className="notifications" image="https://www.shareicon.net/data/512x512/2017/02/15/878700_alert_512x512.png"></IconBtn>
+            <IconBtn className="search2" image="https://cdn.iconscout.com/icon/free/png-256/free-search-1990-1174913.png"></IconBtn>
+        </section>
+      </section>
+      </>
+    )
+}
