0dccac6198bb3473518b8211aec589d04e97552d
Santiago Perez <santipr2004@gmail.com>
2024-02-09 17:00:24 -0500

    Feat: NavBar celphone style complete

--- Lab1/src/components/NavBarCel/NavBarCel.jsx
+++ Lab1/src/components/NavBarCel/NavBarCel.jsx
@@ -5,14 +5,14 @@ import "./NavBarCel.css"
 export default function NavBarCel(){
     return(
       <>
-      <section className="mainphone">
-        <section className="left">
-            <IconBtn className="yt" image="https://ongpng.com/wp-content/uploads/2023/04/3.youtube-logo-2416x776-1.png"></IconBtn>
+      <section className="main-phone">
+        <section className="left-phone">
+            <IconBtn clas="yt-phone" image="https://ongpng.com/wp-content/uploads/2023/04/3.youtube-logo-2416x776-1.png"></IconBtn>
         </section>
-        <section className="right">
-            <IconBtn className="share" image="https://static.thenounproject.com/png/3388431-200.png"></IconBtn>
-            <IconBtn className="notifications" image="https://www.shareicon.net/data/512x512/2017/02/15/878700_alert_512x512.png"></IconBtn>
-            <IconBtn className="search2" image="https://cdn.iconscout.com/icon/free/png-256/free-search-1990-1174913.png"></IconBtn>
+        <section className="right-phone">
+            <IconBtn clas="share-phone" image="https://static.thenounproject.com/png/3388431-200.png"></IconBtn>
+            <IconBtn clas="notifications-phone" image="https://www.shareicon.net/data/512x512/2017/02/15/878700_alert_512x512.png"></IconBtn>
+            <IconBtn clas="search-phone" image="https://cdn.iconscout.com/icon/free/png-256/free-search-1990-1174913.png"></IconBtn>
         </section>
       </section>
       </>
