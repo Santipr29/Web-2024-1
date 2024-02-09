9d238472a5da8a0d9aca4a04f01299cc36f83bba
Santiago Perez <santipr2004@gmail.com>
2024-02-09 17:08:32 -0500

    Feat: App content organization for cellphone

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -3,13 +3,14 @@ import NavBar from './components/NavBar/NavBar'
 import MainSections from './components/MainSections/MainSections'
 import VideoSection from './components/VideoSection/VideoSection'
 import NavBarCel from './components/NavBarCel/NavBarCel'
+import "./App.css"
 
 export default function App(){
     return(
       <>
       <NavBarCel></NavBarCel>
       <NavBar></NavBar>
-      <section>
+      <section className="content">
         <MainSections></MainSections>
         <VideoSection></VideoSection>
       </section>
