e0022a819c1cd8d9d4d4a28735e196016e16f426
Santiago Perez <santipr2004@gmail.com>
2024-02-12 22:40:10 -0500

    Feat: sections bar style mid

--- Lab1/src/components/SectionBtn/SectionBtn.jsx
+++ Lab1/src/components/SectionBtn/SectionBtn.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import "./SectionBtn.css"
 
 export function SectionBtn(props){
     const {image, title} = props
     return(
         <button>
-            <img src={image}></img>
+            <img className="section-img" src={image}></img>
             <p>{title}</p>
         </button>
     )
