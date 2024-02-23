ab384e7ba85c2ac3f037c6b3147eb586205ebcba
Santiago Perez <santipr2004@gmail.com>
2024-02-23 13:20:29 -0500

    Feat: Hamburguer menu width finished

--- Lab1/src/App.jsx
+++ Lab1/src/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import {useState} from 'react'
-import NavBar from './components/NavBar/NavBar'
-import MainSections from './components/MainSections/MainSections'
-import VideoSection from './components/VideoSection/VideoSection'
-import NavBarCel from './components/NavBarCel/NavBarCel'
-import MoreOptions from './components/MoreOptions/MoreOptions'
-import "./App.css"
+import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar/NavBar';
+import MainSections from './components/MainSections/MainSections';
+import VideoSection from './components/VideoSection/VideoSection';
+import NavBarCel from './components/NavBarCel/NavBarCel';
+import MoreOptions from './components/MoreOptions/MoreOptions';
+import "./App.css";
 
-export default function App(){
-  const [showMenu, setShowMenu] = useState(false)
+export default function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1023);
 
-  const handleShowMenu = (e) => {
-    setShowMenu(!showMenu)
-  }
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    function handleResize() {
+      setIsWideScreen(window.innerWidth > 1023);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const videoSectionWidth = isWideScreen ? (showMenu ? 'calc(100% - 240px)' : 'calc(100% - 100px)') : '100%';
 
     return(
       <>
@@ -24,7 +36,7 @@ export default function App(){
           ? <MoreOptions></MoreOptions>
           : <MainSections></MainSections>
         }
-        <VideoSection widht={ showMenu ? 'calc(100% - 240px)' : 'calc(100% - 90px)' }></VideoSection> 
+        <VideoSection widht={ videoSectionWidth }></VideoSection> 
       </section>
       </>
     )
