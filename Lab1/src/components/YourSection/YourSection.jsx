abe152d490cb6805090b8846d95bff370b553e6d
Santiago Perez <santipr2004@gmail.com>
2024-02-24 07:20:45 -0500

    Feat: Your section for hamburger menu style finish

--- Lab1/src/components/YourSection/YourSection.jsx
+++ Lab1/src/components/YourSection/YourSection.jsx
@@ -10,9 +10,9 @@ export default function YourSection(){
         <section className="btn-section">
           <SectionBtn image="https://static.thenounproject.com/png/578286-200.png" title="Your channel" uid="sections-options"></SectionBtn>
           <SectionBtn image="https://cdn-icons-png.flaticon.com/512/2961/2961948.png" title="History" uid="sections-options"></SectionBtn>
-          <SectionBtn image="https://static.thenounproject.com/png/4693940-200.png" title="Your videos" uid="sections-options"></SectionBtn>
+          <SectionBtn image="https://static.thenounproject.com/png/4693940-200.png" title="Your videos" uid="sections-options2"></SectionBtn>
           <SectionBtn image="https://static-00.iconduck.com/assets.00/time-icon-256x256-3vd21cb8.png" title="Watch later" uid="sections-options"></SectionBtn>
-          <SectionBtn image="https://www.iconpacks.net/icons/1/free-scissors-icon-981-thumb.png" title="Your clips" uid="sections-options"></SectionBtn>
+          <SectionBtn image="https://www.iconpacks.net/icons/1/free-scissors-icon-981-thumb.png" title="Your clips" uid="sections-options2"></SectionBtn>
           <SectionBtn image="https://www.freeiconspng.com/uploads/like-icon-0.png" title="Liked videos" uid="sections-options"></SectionBtn>
         </section>
       </section>
