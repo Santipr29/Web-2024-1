916d185bd21df902caed7b308401b6d708fd8f9d
Santiago Perez <santipr2004@gmail.com>
2024-02-24 07:46:27 -0500

    Feat: Config section for hamburger menu style finish

--- Lab1/src/components/ConfigSection/ConfigSection.jsx
+++ Lab1/src/components/ConfigSection/ConfigSection.jsx
@@ -5,11 +5,13 @@ import "./ConfigSection.css"
 export default function ConfigSection(){
     return(
       <>
+      <section className="config-section">
       <section className="btn-section">
       <SectionBtn image="https://static-00.iconduck.com/assets.00/settings-icon-2048x2046-cw28eevx.png" title="Settings" uid="sections-options"></SectionBtn>
       <SectionBtn image="https://images.freeimages.com/fic/images/icons/2770/ios_7_icons/512/flag.png" title="Report history" uid="sections-options"></SectionBtn>
       <SectionBtn image="https://static.thenounproject.com/png/1829242-200.png" title="Help" uid="sections-options"></SectionBtn>
       <SectionBtn image="https://static.thenounproject.com/png/4808951-200.png" title="Send feedback" uid="sections-options"></SectionBtn>
+      </section>
       </section>
       </>
     )
