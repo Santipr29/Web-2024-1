6116eea00ddfdb31cf4bdff3d2fe59bc2335eb8d
Santiago Perez <santipr2004@gmail.com>
2024-02-24 07:24:58 -0500

    Feat: subs section for hamburger menu styles finished

--- Lab1/src/components/SubsSection/SubsSection.jsx
+++ Lab1/src/components/SubsSection/SubsSection.jsx
@@ -8,13 +8,13 @@ export default function SubsSection(){
       <section className="subs-section">
       <h2>Subscriptions</h2>
       <section className="btn-section">
-      <SectionBtn image="https://yt3.googleusercontent.com/ytc/AIf8zZSmNKhvFf2fL7E8IF5pX_E3a1tE4BoigztO1KZSUF0=s176-c-k-c0x00ffffff-no-rj-mo" title="Pablots" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://yt3.ggpht.com/_cJUz0aSSV6TtTrw4_XaqsMVjbMkddQxxLxnN2G_PJ9NZv-9sjVXC88ILPd7kQRkO8OO7YG7=s176-c-k-c0x00ffffff-no-rj-mo" title="NBA" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://yt3.ggpht.com/4XDYWC0nsWGrDn__8NnDypC-5eS2gfhfst273Y_Huf1gnmOJ05zKH8uJlYAp7ejBpjX08lFoHOI=s176-c-k-c0x00ffffff-no-rj-mo" title="FC Barcelona" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://yt3.ggpht.com/3ondwJtqgg_1YlfjFd_KJMQ-Q4bz764TiYlsZc0kKwsU5FQghNUwalM0jnnPo7WysTCOWkQQIg=s176-c-k-c0x00ffffff-no-rj-mo" title="ESPN Fans" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://yt3.ggpht.com/pk-jnYnbdNBUKEtBdc3buYbCM0Xu0_TDcVfYON1qAez3CVPbA-FWGyCbF4BaRF1T9MqxjG-mugk=s176-c-k-c0x00ffffff-no-rj-mo" title="Ibai" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://yt3.ggpht.com/U9x-_BMoXJBPf9RWEab_TEu2TX1o1plDjeY52_BQVshkLYDtiQkCo3MVZsmq5X0gf0cTvRkFig=s176-c-k-c0x00ffffff-no-rj-mo" title="Man City" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://yt3.googleusercontent.com/F9tU0eTy96fqLC1c8SGgU8GNqyRiyEQttOuY2epuCDr2XhyFoaMecugxA6kcUrEMqZL4HjnPUg=s176-c-k-c0x00ffffff-no-rj-mo" title="Dei V" uid="sections-options"></SectionBtn>
+      <SectionBtn image="https://yt3.googleusercontent.com/ytc/AIf8zZSmNKhvFf2fL7E8IF5pX_E3a1tE4BoigztO1KZSUF0=s176-c-k-c0x00ffffff-no-rj-mo" title="Pablots" uid="sections-subs"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/_cJUz0aSSV6TtTrw4_XaqsMVjbMkddQxxLxnN2G_PJ9NZv-9sjVXC88ILPd7kQRkO8OO7YG7=s176-c-k-c0x00ffffff-no-rj-mo" title="NBA" uid="sections-subs"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/4XDYWC0nsWGrDn__8NnDypC-5eS2gfhfst273Y_Huf1gnmOJ05zKH8uJlYAp7ejBpjX08lFoHOI=s176-c-k-c0x00ffffff-no-rj-mo" title="FC Barcelona" uid="sections-subs"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/3ondwJtqgg_1YlfjFd_KJMQ-Q4bz764TiYlsZc0kKwsU5FQghNUwalM0jnnPo7WysTCOWkQQIg=s176-c-k-c0x00ffffff-no-rj-mo" title="ESPN Fans" uid="sections-subs"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/pk-jnYnbdNBUKEtBdc3buYbCM0Xu0_TDcVfYON1qAez3CVPbA-FWGyCbF4BaRF1T9MqxjG-mugk=s176-c-k-c0x00ffffff-no-rj-mo" title="Ibai" uid="sections-subs"></SectionBtn>
+      <SectionBtn image="https://yt3.ggpht.com/U9x-_BMoXJBPf9RWEab_TEu2TX1o1plDjeY52_BQVshkLYDtiQkCo3MVZsmq5X0gf0cTvRkFig=s176-c-k-c0x00ffffff-no-rj-mo" title="Man City" uid="sections-subs"></SectionBtn>
+      <SectionBtn image="https://yt3.googleusercontent.com/F9tU0eTy96fqLC1c8SGgU8GNqyRiyEQttOuY2epuCDr2XhyFoaMecugxA6kcUrEMqZL4HjnPUg=s176-c-k-c0x00ffffff-no-rj-mo" title="Dei V" uid="sections-subs"></SectionBtn>
       </section>
       </section>
       </>
