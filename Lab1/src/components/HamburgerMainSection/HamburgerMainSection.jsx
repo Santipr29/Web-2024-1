034d201f66e694080f47a63d2a456be25d536aa9
Santiago Perez <santipr2004@gmail.com>
2024-02-24 06:59:34 -0500

    Feat: Hamburger main section style finish

--- Lab1/src/components/HamburgerMainSection/HamburgerMainSection.jsx
+++ Lab1/src/components/HamburgerMainSection/HamburgerMainSection.jsx
@@ -6,10 +6,12 @@ export default function HamburguerMainSection(){
 
     return(
       <>
-      <section className="hamburger-main-section">
-      <SectionBtn image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Home-icon.svg/800px-Home-icon.svg.png" title="Home" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://freelogopng.com/images/all_img/1685029929youtube-shorts-logo-black.png" title="Shorts" uid="sections-options"></SectionBtn>
-      <SectionBtn image="https://cdn.icon-icons.com/icons2/3237/PNG/512/menu_youtube_social_media_subs_subscription_icon_197393.png" title="Subscriptions" uid="sections-options"></SectionBtn>
+      <section className="hamburger-container-section">
+        <section className="hamburger-main-section">
+          <SectionBtn image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Home-icon.svg/800px-Home-icon.svg.png" title="Home" uid="sections-options"></SectionBtn>
+          <SectionBtn image="https://freelogopng.com/images/all_img/1685029929youtube-shorts-logo-black.png" title="Shorts" uid="sections-options"></SectionBtn>
+          <SectionBtn image="https://cdn.icon-icons.com/icons2/3237/PNG/512/menu_youtube_social_media_subs_subscription_icon_197393.png" title="Subscriptions" uid="sections-options"></SectionBtn>
+        </section>
       </section>
       </>
     )
